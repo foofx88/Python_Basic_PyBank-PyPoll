a31cbfd1d4b57f1a12553431ad4d5d940bc4e7b8
foofx <fangxuan.foo@gmail.com>
2021-03-27 12:42:36 +0800

    finalised codes with comments

--- pypoll/election_results.py
+++ pypoll/election_results.py
@@ -6,31 +6,31 @@ elecdata_csv = os.path.join(dirname, 'Resources', 'election_data.csv')
 
 with open(elecdata_csv, 'r') as csvfile:
 
-    csvreader = csv.reader(csvfile, delimiter=',')
+    csvreader = csv.reader(csvfile, delimiter=',')  # Split the data on commas
     total_votes = 0
     candidates = []
-    votes=[]
-    candidate_votes = []
+    votes=[]                                        #initialise all value on variables
+    candidate_votes = []                            #used list for most of the variables as there are multiple data
     candidate_vote_perc = []
     winner = ""
     next(csvfile)
 
-    for row in csvreader: 
+    for row in csvreader:                           #first loop to get total votes
         total_votes += 1
 
-        if row[2] not in candidates:
+        if row[2] not in candidates:                #and get unique individual candidates in the csv 
             candidates.append(row[2])
 
-        votes.append(row[2])
+        votes.append(row[2])                        #adds number of votes accodingly, without this the next loop won't work
 
-    for individual in candidates:
+    for individual in candidates:                   #second loop to count all votes for each individual
         candidate_votes.append(votes.count(individual))
-        candidate_vote_perc.append(round(votes.count(individual)/total_votes*100, 2))
+        candidate_vote_perc.append(round(votes.count(individual)/total_votes*100, 2))   #as well as the percentage
         
             
-winner = candidates[candidate_votes.index(max(candidate_votes))]
+winner = candidates[candidate_votes.index(max(candidate_votes))]    #get the winner using MAX within the index
     
-    
+#time to print out the result on the terminal    
 print(f'Election Results')
 print(f'---------------------')
 print(f'Total Votes: {total_votes}')
@@ -39,7 +39,7 @@ for i in range (len(candidates)):
 print(f'---------------------')
 print(f'Winner: {winner} ')
 
-
+#the following is to output the result to a text file - output.txt
 out = open("C:/Users/bluec/python-challenge/PyPoll/analysis/output.txt", "w")
 out.write(f'Election Results\n')
 out.write(f'---------------------\n')
